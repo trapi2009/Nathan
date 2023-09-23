cba528df46f793ff2cd4042756e1fc6364594b8e
trapi2009 <78277072+trapi2009@users.noreply.github.com>
2023-09-22 18:14:43 -0700

    Add DarkLight.js code

--- DarkLight.js
+++ DarkLight.js
@@ -1,1 +1,19 @@
+function toggleDarkMode() {
+	const darkModeToggle = document.getElementById("dark-mode");
+	const container = document.querySelector(".toggle-container");
+	if (darkModeToggle.checked) {
+		document.body.classList.add("dark-mode");
+		container.title = "Switch to Light Mode";
+	} else {
+		document.body.classList.remove("dark-mode");
+		container.title = "Switch to Dark Mode";
+	}
+}
 
+// Set initial mode based on system preference
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+	const darkModeToggle = document.getElementById("dark-mode");
+	darkModeToggle.checked = true;
+	document.body.classList.add("dark-mode");
+	document.querySelector(".toggle-container").title = "Switch to Light Mode";
+}
